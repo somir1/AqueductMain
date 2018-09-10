669058efb1bb406f72f105e033ad09ca0dcdc46a
somir1 <33099952+somir1@users.noreply.github.com>
2018-09-10 11:34:14 -0400

    new2

--- Aqueduct/Assets/Scripts/AqueductManager.cs
+++ Aqueduct/Assets/Scripts/AqueductManager.cs
@@ -21,6 +21,11 @@ public class AqueductManager : MonoBehaviour
     public Transform horizontalPrefab;
     public Transform targetObject;
 
+<<<<<<< HEAD
+=======
+    public List<GameObject> floorList= new List<GameObject>(); 
+
+>>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
     // Use this for initialization
     void Start()
     {
