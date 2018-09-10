bb222ce2ded373a2769dbe0457cbce4c55daa5e0
somir1 <33099952+somir1@users.noreply.github.com>
2018-09-10 11:37:23 -0400

    merge

--- Aqueduct/Assets/Liquid2D/LiquidGenarate.cs
+++ Aqueduct/Assets/Liquid2D/LiquidGenarate.cs
@@ -15,7 +15,11 @@ public class LiquidGenarate : MonoBehaviour {
 			float x = Random.Range (-1.0f, 1.0f) * _width / 2;
 			float y = Random.Range (-1.0f, 1.0f) * _height / 2;
 <<<<<<< HEAD
+<<<<<<< HEAD
 			temp.transform.localPosition = new Vector3 (x, y, 0);
+=======
+			temp.transform.localPosition = new Vector3 (x+3, y+3, 0);
+>>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
 =======
 			temp.transform.localPosition = new Vector3 (x+3, y+3, 0);
 >>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
