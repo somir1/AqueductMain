23cf0dfcabd8a735cfe60ec464d6679400bb0b14
somir1 <33099952+somir1@users.noreply.github.com>
2018-09-10 12:18:46 -0400

    cx

--- Aqueduct/Assets/Scripts/AqueductManager.cs
+++ Aqueduct/Assets/Scripts/AqueductManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AqueductManager : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class AqueductManager : MonoBehaviour
     int ductR = 0;
     int ductD = 3;
     int ductH = 0;
+
+    int Count=0;
 
     Vector3 mousePosition, targetPosition;
 
@@ -21,55 +24,28 @@ public class AqueductManager : MonoBehaviour
     public Transform horizontalPrefab;
     public Transform targetObject;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
+    public GameObject Bottom;
 
->>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
-=======
     public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of ccb7ec3... Merge branch 'master' of https://github.com/somir1/AqueductMain
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of bfce78a... Distance Sript Added
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of bfce78a... Distance Sript Added
-=======
-    public List<GameObject> floorList= new List<GameObject>(); 
-
->>>>>>> parent of bfce78a... Distance Sript Added
+    
     // Use this for initialization
     void Start()
     {
-       
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("Ground"))
+        {
+            floorList.Add(fooObj);
+        }
+
+        floorList = floorList.OrderBy(t => t.name).ToList();
+
+        foreach (GameObject floor in floorList) {
+           // Debug.Log(floor.name);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() { 
+    
         if (Input.GetKeyDown("1"))
         {
             Debug.Log("Duct Left: " + ductL);
@@ -106,7 +82,7 @@ public class AqueductManager : MonoBehaviour
                 ductL--;
                 Debug.Log(ductL);
                 Instantiate(targetObject, targetObject.transform.position, targetObject.transform.rotation);
-
+                Count++;
             }
 
             else if (targetObject == rightPrefab && ductR > 0)
@@ -114,7 +90,7 @@ public class AqueductManager : MonoBehaviour
                 ductR--;
                 Debug.Log(ductR);
                 Instantiate(targetObject, targetObject.transform.position, targetObject.transform.rotation);
-
+                Count++;
             }
 
             else if (targetObject == horizontalPrefab && ductH > 0)
@@ -122,7 +98,7 @@ public class AqueductManager : MonoBehaviour
                 ductH--;
                 Debug.Log(ductH);
                 Instantiate(targetObject, targetObject.transform.position, targetObject.transform.rotation);
-
+                Count++;
             }
 
             else if (targetObject == downPrefab && ductD > 0)
@@ -130,9 +106,10 @@ public class AqueductManager : MonoBehaviour
                 ductD--;
                 Debug.Log(ductD);
                 Instantiate(targetObject, targetObject.transform.position, targetObject.transform.rotation);
-
+                Count++;
             }
 
+            //floorList[Count].GetComponent;
         }
     }
 }
---
+++ Aqueduct/Assets/Scripts/MyScript.cs
@@ -0,0 +1,9 @@
+﻿//Attach a script like this to a Sprite GameObject (Create>2D Object>Sprite). Assign a Sprite to it in the Sprite field.
+//Repeat the first step for another two Sprites and make them overlap each other slightly. This shows how the order number changes the view of the Sprites.
+
+using UnityEngine;
+public class MyScript : MonoBehaviour
+{
+    public int MyOrder;
+    public string MyName;
+}
