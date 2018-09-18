96fb41a2d3d7c8ae48b793030c53d5b10cbdfe3f
Edwin Sanchez <zehcnas97@gmail.com>
2018-09-18 17:15:59 -0400

    Tracking Liquid Both Sides
    Tracking liquid for both sides works, but deleting water creates an error.

---
+++ Aqueduct/Assets/Scripts/RightLiquidLost.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RightLiquidLost : MonoBehaviour
+{
+
+    public LeftWaterTracker rightLiquidLost;
+    public LiquidGenarate liquidGenarate;
+    public float StartWater;
+    public float LeftTriggerWaterLoss;
+    public float RightTriggerWaterLoss;
+    public float TotalLiquidLost;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        StartWater = liquidGenarate._numLiquid;
+    }
+
+
+    private void Update()
+    {
+        RightTriggerWaterLoss = rightLiquidLost.RightTriggerWaterLoss;
+        TotalLiquidLost = LeftTriggerWaterLoss + RightTriggerWaterLoss;
+        LoseCondition();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Water")
+        {
+            LeftTriggerWaterLoss++;
+            //Removing comment from Destroy(col.gameObject) creates an error in TransformFollower.cs 
+            //Destroy(col.gameObject);
+        }
+    }
+
+    public void LoseCondition()
+    {
+        //Debug.Log("You lost " + LeftTriggerWaterLoss + " water from the right. And "
+        //+ RightTriggerWaterLoss + " from the left. " + "You lose a total of " + TotalLiquidLost + "water");
+
+        if (TotalLiquidLost >= (StartWater * .5) - .5)
+        {
+            SceneManager.LoadScene("Lose");
+        }
+    }
+
+}
