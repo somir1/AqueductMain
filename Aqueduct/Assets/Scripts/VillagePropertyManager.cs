f405239e2b15c6fa3e136e4dfa29a4734c7a04fe
jose34953 <33074171+jose34953@users.noreply.github.com>
2018-10-15 18:41:24 -0400

    Hud stock circle changes color
    village property logic started

---
+++ Aqueduct/Assets/Scripts/VillagePropertyManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VillagePropertyManager : MonoBehaviour {
+
+    public List<GameObject> VillageItems = new List<GameObject>();
+
+    public float propertyDamage = 0;
+    public int totalItems = 0;
+    public int numItemsLost = 0;
+    float maxLoss = 0;
+
+    void Start () {
+
+        foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("Village"))
+        {
+            VillageItems.Add(fooObj);
+        }
+
+        totalItems = VillageItems.Count;
+
+        maxLoss = totalItems / 2;
+        
+    }
+	
+	void Update () {
+
+       // propertyDamage = numItemsLost / totalItems;
+        //Debug.Log(propertyDamage);
+        //if (propertyDamage <= maxLoss) {
+          //  SceneManager.LoadScene("Lose");
+        //}
+	}
+}
