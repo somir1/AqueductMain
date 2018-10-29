7c1f52e0b7b83356f70a956ee829f5597c1d808e
jose34953 <33074171+jose34953@users.noreply.github.com>
2018-10-29 15:55:17 -0400

    data persistance started

---
+++ Aqueduct/Assets/Scripts/GameControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : MonoBehaviour {
+
+    public GameControl control;
+
+    public int level1;
+    public int level2;
+    public int level3;
+
+    private void Awake()
+    {
+        if (control == null) {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } else if (control != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+    public void Save() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+        PlayerData data = new PlayerData();
+
+        data.level1 = level1;
+        data.level2 = level2;
+        data.level3 = level3;
+    }
+}
+
+[Serializable]
+class PlayerData {
+    public int level1;
+    public int level2;
+    public int level3;
+}
