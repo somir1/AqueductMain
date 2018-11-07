983274d4a8ae1b81b8b0a76ebc14cc11c1e86eaa
Edwin Sanchez <zehcnas97@gmail.com>
2018-11-07 01:01:27 -0500

    Tutorial
    Duplicated 1st level to create tutorial with in text pop ups. Some code is in but still incomplete, but UI is set up. Still need to scale to proper size.

---
+++ Aqueduct/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this makes it so we can edit this in the engine. 
+[System.Serializable]
+public class Dialogue {
+    //name of NPC
+    public string name;
+    //sentences you are loading into the queue
+    [TextArea(3,10)]
+    public string[] sentences;
+    
+
+}
