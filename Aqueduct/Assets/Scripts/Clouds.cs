74c867ca1712f7800306b264c64158b058e7f034
jose34953 <33074171+jose34953@users.noreply.github.com>
2018-11-26 21:24:45 -0500

    rain sexy

---
+++ Aqueduct/Assets/Scripts/Clouds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour
+{
+
+    void Update()
+    {
+       
+        transform.position = new Vector2(Mathf.Lerp(transform.position.x, transform.position.x-10, Random.Range(0.04f, 0.08f) * Time.deltaTime), transform.position.y);
+
+    }
+}
